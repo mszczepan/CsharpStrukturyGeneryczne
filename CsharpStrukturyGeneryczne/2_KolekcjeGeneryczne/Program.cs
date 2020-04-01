668c43e824a59022bbbd35e7b4fa4adde3a213ff
Mateusz <szczepan.mateusz@gmail.com>
2020-04-01 22:25:33 +0200

    2_6 KolekcjeGeneryczne - Stos+StosTest

--- CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
@@ -17,12 +17,30 @@ namespace _2_KolekcjeGeneryczne
             kolejka.Enqueue(new Pracownik { Imie = "Jacek", Nazwisko = "Nowak" });
             kolejka.Enqueue(new Pracownik { Imie = "Ola", Nazwisko = "Nowak" });
 
-
+            Console.WriteLine("Koleka");
             while (kolejka.Count>0)
             {
                 var  pracownik= kolejka.Dequeue();
                 Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
             }
+
+
+            Stack<Pracownik> stos = new Stack<Pracownik>();
+            stos.Push(new Pracownik { Imie = "Marcin", Nazwisko = "Nowak" });
+            stos.Push(new Pracownik { Imie = "Tomek", Nazwisko = "Nowak" });
+            stos.Push(new Pracownik { Imie = "Jacek", Nazwisko = "Nowak" });
+            stos.Push(new Pracownik { Imie = "Ola", Nazwisko = "Nowak" });
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Stos");
+            while (stos.Count > 0)
+            {
+                var pracownik = stos.Pop();
+                Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
+            }
+
+
+
         }       
     }
 }
---
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczneTest/StosTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2_KolekcjeGeneryczneTest
+{
+    [TestClass]
+    public class StosTest
+    {
+        [TestMethod]
+        public void UzyciePeek()
+        {
+            var stos = new Stack<int>();
+
+            stos.Push(1);
+            stos.Push(2);
+            stos.Push(3);
+            stos.Push(4);
+
+            Assert.AreEqual(4, stos.Peek());
+        }
+
+        [TestMethod]
+        public void UzycieContains()
+        {
+            var stos = new Stack<int>();
+
+            stos.Push(1);
+            stos.Push(2);
+            stos.Push(3);
+            stos.Push(4);
+
+            Assert.IsTrue(stos.Contains(4));
+        }
+        [TestMethod]
+        public void UzycieMetodyToArray()
+        {
+            //przeglądanie stos poprzez rzutowanie na tablice
+            var stos = new Stack<int>();
+            stos.Push(1);
+            stos.Push(2);
+            stos.Push(3);
+            stos.Push(4);
+
+            var tablica = stos.ToArray();
+
+            stos.Pop();
+            Assert.AreEqual(4, tablica[0]);
+            Assert.AreEqual(3, stos.Count);
+
+
+        }
+
+
+        [TestMethod]
+        public void UzycieClear()
+        {
+            var stos = new Stack<int>();
+
+            stos.Push(1);
+            stos.Push(2);
+            stos.Push(3);
+            stos.Push(4);
+
+            stos.Clear();
+            Assert.AreEqual(0,stos.Count);
+        }
+    }
+}
