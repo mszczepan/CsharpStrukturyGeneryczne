7620b146750c9ea5b86b0acc487da6c1ed1c276e
Mateusz <szczepan.mateusz@gmail.com>
2020-04-04 12:29:08 +0200

    2_9 KolekcjeGeneryczne - LinkedList

--- CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
@@ -10,10 +10,50 @@ namespace _2_KolekcjeGeneryczne
     {
         static void Main(string[] args)
         {
-            Kolejka();
+            //Kolejka();
+            //Stos();
+            //HashSet();
+            //LinkedList();
 
-            Stos();
-            HashSet();
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(5);
+            list.AddFirst(6);
+            list.AddFirst(7);
+
+            // odniesienie do pierwszego elementu listy
+            var elementPierwszy = list.First;
+            var elementOstatni = list.Last;
+
+            list.AddAfter(elementPierwszy, 10);
+            list.AddBefore(elementPierwszy, 20);
+
+            var wezel = list.First;
+
+            while (wezel != null)
+            {
+                Console.WriteLine(wezel.Value);
+                wezel = wezel.Next;
+
+            }
+
+        }
+
+        private static void LinkedList()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(5);
+            list.AddFirst(6);
+            list.AddFirst(7);
+            list.AddLast(9);
+            list.AddLast(2);
+
+
+
+
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
         }
 
         private static void HashSet()
